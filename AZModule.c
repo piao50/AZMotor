6070607027d7cfec40e660a7475a8214edef6ffc
gushi <piao50@hotmail.com>
2018-03-20 10:01:23 +0800

    gushi

--- AZModule.c
+++ AZModule.c
@@ -39,55 +39,57 @@ int main(int argc, char *argv[])
   }
 
   sleep(1);
+
+  print("bye gushi!\r\n");
   
-  int delay = 2000;
-  int loop = 10000;
-  /* multiply register */
-  while(loop-- > 0)
-    {
-      modbus_set_slave(ctx, loop % 2 == 0 ? 1 : 2);
-      printf("--------------------\r\n");
-      //      sleep(5);
-      //      uint16_t UT_REGISTERS_TAB[] = { 0x0000, 0x0008 };
-      //      rc = modbus_write_registers(ctx, 0x007c, 0x0002, UT_REGISTERS_TAB);
-      //      if (rc == -1) {
-      //	printf("FAIL: write registers, rc=%d.\r\n", rc);
-      //      }
+ //  int delay = 2000;
+ //  int loop = 10000;
+ //  /* multiply register */
+ //  while(loop-- > 0)
+ //    {
+ //      modbus_set_slave(ctx, loop % 2 == 0 ? 1 : 2);
+ //      printf("--------------------\r\n");
+ //      //      sleep(5);
+ //      //      uint16_t UT_REGISTERS_TAB[] = { 0x0000, 0x0008 };
+ //      //      rc = modbus_write_registers(ctx, 0x007c, 0x0002, UT_REGISTERS_TAB);
+ //      //      if (rc == -1) {
+ //      //	printf("FAIL: write registers, rc=%d.\r\n", rc);
+ //      //      }
 
-      uint16_t cmd[2];
-      cmd[0] = 0x0000; cmd[1] = 0x0002;
-      if(-1 == modbus_write_registers(ctx,0x1800,0x0002,cmd))
-      {printf("ERR:%d\r\n", errno);}
+ //      uint16_t cmd[2];
+ //      cmd[0] = 0x0000; cmd[1] = 0x0002;
+ //      if(-1 == modbus_write_registers(ctx,0x1800,0x0002,cmd))
+ //      {printf("ERR:%d\r\n", errno);}
 
-      cmd[0] = 0x0000; cmd[1] = 0x0140;
-      if(-1 == modbus_write_registers(ctx,0x1802,0x0002,cmd))
-      {printf("ERR:%d\r\n", errno);}
+ //      cmd[0] = 0x0000; cmd[1] = 0x0140;
+ //      if(-1 == modbus_write_registers(ctx,0x1802,0x0002,cmd))
+ //      {printf("ERR:%d\r\n", errno);}
 
-      cmd[0] = 0x0000; cmd[1] = 0x00ff;
-      if(-1 == modbus_write_registers(ctx,0x1804,0x0002,cmd))
-      {printf("ERR:%d\r\n",errno);}
+ //      cmd[0] = 0x0000; cmd[1] = 0x00ff;
+ //      if(-1 == modbus_write_registers(ctx,0x1804,0x0002,cmd))
+ //      {printf("ERR:%d\r\n",errno);}
 
-      cmd[0] = 0x0000; cmd[1] = 0x05dc;
-      if(-1 == modbus_write_registers(ctx,0x1806,0x0002,cmd))
-      {printf("ERR:%d\r\n",errno);}
+ //      cmd[0] = 0x0000; cmd[1] = 0x05dc;
+ //      if(-1 == modbus_write_registers(ctx,0x1806,0x0002,cmd))
+ //      {printf("ERR:%d\r\n",errno);}
 
-      cmd[0] = 0x0000; cmd[1] = 0x05dc;
-      if(-1 == modbus_write_registers(ctx,0x1808,0x0002,cmd))
-      {printf("ERR:%d\r\n",errno);}
+ //      cmd[0] = 0x0000; cmd[1] = 0x05dc;
+ //      if(-1 == modbus_write_registers(ctx,0x1808,0x0002,cmd))
+ //      {printf("ERR:%d\r\n",errno);}
 
-      cmd[0] = 0x0000; cmd[1] = 0x0008;
-      if(-1 == modbus_write_registers(ctx,0x007c,0x0002,cmd))
-      {printf("ERR:%d\r\n",errno);}
+ //      cmd[0] = 0x0000; cmd[1] = 0x0008;
+ //      if(-1 == modbus_write_registers(ctx,0x007c,0x0002,cmd))
+ //      {printf("ERR:%d\r\n",errno);}
 
-      cmd[0] = 0x0000; cmd[1] = 0x0000;
-      if(-1 == modbus_write_registers(ctx,0x007c,0x0002,cmd))
-      {printf("ERR:%d\r\n",errno);}
+ //      cmd[0] = 0x0000; cmd[1] = 0x0000;
+ //      if(-1 == modbus_write_registers(ctx,0x007c,0x0002,cmd))
+ //      {printf("ERR:%d\r\n",errno);}
 
-      printf("waiting %d ms.\r\n", delay);
-      if(loop % 2 == 0) usleep(delay*1000);
-      if(-1 == modbus_write_register(ctx,0x007d,0x0010))
-	{printf("ERR:%d\r\n",errno);}
-    }
+ //      printf("waiting %d ms.\r\n", delay);
+ //      if(loop % 2 == 0) usleep(delay*1000);
+ //      if(-1 == modbus_write_register(ctx,0x007d,0x0010))
+	// {printf("ERR:%d\r\n",errno);}
+ //    }
 
   /*
   req_length = modbus_send_raw_request(ctx, raw_1, 6 * sizeof(uint8_t));
