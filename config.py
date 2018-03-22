8d86a1c35ed9b92c9313d22fda67e2b6434b9348
Gu Shi <piao50@gmail.com>
2018-03-22 02:02:34 +0000

    gushi

--- config.py
+++ config.py
@@ -14,14 +14,9 @@ data = {}
 for i in range(0,11):
     data[str(i).zfill(2)] = {'x':1,'y':2,'z':3}
 
-#print data
-
-for key in sorted(data.iterkeys()):
-    print "%s: %s" % (key, data[key])
-
 with open('xyz_motor.json','w') as f:
     s = json.dumps(data,
-#                     indent=4,
+                     indent=4,
                      separators=(',',':'),
                      ensure_ascii=False)
     f.write(to_unicode(s))
