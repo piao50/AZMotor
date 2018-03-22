26bd57a8cfc3894e4c6d8da6571c9331e809083f
Gu Shi <piao50@gmail.com>
2018-03-22 03:02:19 +0000

    gushi

--- config.py
+++ config.py
@@ -5,16 +5,23 @@ import collections
 
 import io
 try:
+    
     to_unicode = unicode
 except NameError:
+
+    
     to_unicode = str
 
 print 'hello, gushi'
 
 data = collections.OrderedDict()
 
-for i in range(0,11):
-    data[str(i).zfill(2)] = {'1':1,'2':2,'3':3}
+for i in range(0,5):
+    row = collections.OrderedDict()
+    row['1'] = ','.join(['1',str(1000 * i),'1000'])
+    row['2'] = ','.join(['1',str(1000 * i),'1000'])
+    row['3'] = ','.join(['1',str(1000 * i),'1000'])
+    data[str(i).zfill(2)] = row
 
 with open('xyz_motor.json','w') as f:
     s = json.dumps(data,
