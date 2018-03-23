9054480497c875d5977dfe7160a6365668c5ae52
Gu Shi <piao50@gmail.com>
2018-03-23 17:06:50 +0800

    gushi

--- erpi.h
+++ erpi.h
@@ -28,6 +28,11 @@ public:
 		printf("open.\r\n");
 		if(ctx == NULL)
 			ctx = modbus_new_rtu("/dev/ttyUSB0", 115200, 'E', 8, 1);
+		if (modbus_connect(ctx) == -1) {
+			fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+			modbus_free(ctx);
+			return -1;
+		}
 		return 0;
 	}
 	int close()
@@ -42,22 +47,42 @@ public:
 		return 0;
 	}
 
-	int set_position_info(uint16_t slave_id, uint16_t pos_id, uint16_t method, uint16_t position, uint16_t speed, uint16_t current)
+	int set_position_info(uint16_t slave_id, uint32_t pos_id, uint32_t method, uint32_t position, uint32_t speed, uint32_t start_slope, uint32_t stop_slope)
 	{
-		printf("I'm set_position_info.\r\n");
-
+		printf("I'm set_position_info. [%d-(%d)],%d,%d,%d,%d,%d\r\n",
+			slave_id,
+			pos_id,
+			method,
+			position,
+			speed,
+			start_slope,
+			stop_slope);
 		if (ctx == NULL) {
 			fprintf(stderr, "Unable to allocate libmodbus context\n");
 			return -1;
 		}
-		modbus_set_slave(ctx, 2);
-		if (modbus_connect(ctx) == -1) {
-			fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
-			modbus_free(ctx);
-			return -1;
+
+		uint16_t tab_reg[512]; memset(tab_reg, 0, 512);
+		uint16_t base_addr = 0x1800;
+		uint16_t addr_offset = 0x0040;
+		uint16_t addr = base_addr + pos_id * addr_offset;
+
+		tab_reg[0] = (method & 0xFFFF0000) >> 16; tab_reg[1] = method & 0x0000FFFF;
+		tab_reg[2] = (position & 0xFFFF0000) >> 16; tab_reg[3] = position & 0x0000FFFF;
+		tab_reg[4] = (speed & 0xFFFF0000) >> 16; tab_reg[5] = speed & 0x0000FFFF;
+		tab_reg[6] = (start_slope & 0xFFFF0000) >> 16; tab_reg[7] = start_slope & 0x0000FFFF;
+		tab_reg[8] = (stop_slope & 0xFFFF0000) >> 16; tab_reg[9] = stop_slope & 0x0000FFFF;
+
+		for(int i = 0; i < 10; i++)
+		{
+			printf("0x%.*X ", 4, tab_reg[i]);
 		}
 
-
+		modbus_set_slave(ctx, slave_id);
+		if(-1 == modbus_write_registers(ctx, addr, 0x000A, tab_reg)){
+			printf("ERR:%d\r\n", errno);
+			return -1;
+		}
 
 		return 0;
 	}
@@ -66,10 +91,19 @@ public:
 	{
 		modbus_set_slave(ctx, slave_id);
 		printf("I'm goto_position(%d,%d).\r\n", slave_id, pos_id);
-		if(-1 == modbus_write_register(ctx, 0x007D, 0x0008 + (pos_id << 8))){
+
+		uint16_t start_cmd = 0x0008 + (pos_id << 8);
+		// clear alarm reset
+		// start_cmd = start_cmd | 0x0080;
+		uint16_t reset_cmd = 0x0000;
+
+		printf("start_cmd: 0x%.*X\r\n", 4, start_cmd);
+		printf("reset_cmd: 0x%.*X\r\n", 4, reset_cmd);		
+
+		if(-1 == modbus_write_register(ctx, 0x007D, start_cmd)){
 			printf("ERR:%d\r\n", errno);
 		}
-		if(-1 == modbus_write_register(ctx, 0x007D, 0x0000)){
+		if(-1 == modbus_write_register(ctx, 0x007D, reset_cmd)){
 			printf("ERR:%d\r\n",errno);
 		}
 		return 0;
--- erpi_main.cpp
+++ erpi_main.cpp
@@ -14,8 +14,62 @@ int main(int argc, char* argv[])
 
 	m.open();
 
-	m.set_position_info(1, 0, 0, 0, 0, 0);
-	m.goto_position(2, 2);
+	int KHz = 1000;
+
+	m.set_position_info(1, 0, 1, 1000, 8000, 10000*KHz, 1000*KHz);
+	m.set_position_info(1, 1, 1, 2000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 2, 1, 3000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 3, 1, 3000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 4, 1, 4000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 5, 1, 5000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 6, 1, 6000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 7, 1, 7000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 8, 1, 8000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 9, 1, 9000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 10, 1, 10000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 11, 1, 11000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 12, 1, 12000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 13, 1, 13000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 14, 1, 14000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 15, 1, 15000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 16, 1, 16000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 17, 1, 17000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 18, 1, 18000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 19, 1, 19000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(1, 20, 1, 20000, 1000, 5000*KHz, 1000*KHz);
+
+	m.set_position_info(2, 0, 1, 1000, 8000, 10000*KHz, 1000*KHz);
+	m.set_position_info(2, 1, 1, 2000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 2, 1, 3000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 3, 1, 3000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 4, 1, 4000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 5, 1, 5000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 6, 1, 6000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 7, 1, 7000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 8, 1, 8000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 9, 1, 9000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 10, 1, 10000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 11, 1, 11000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 12, 1, 12000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 13, 1, 13000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 14, 1, 14000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 15, 1, 15000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 16, 1, 16000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 17, 1, 17000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 18, 1, 18000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 19, 1, 19000, 1000, 5000*KHz, 1000*KHz);
+	m.set_position_info(2, 20, 1, 20000, 1000, 5000*KHz, 1000*KHz);
+
+	while(1)
+	{
+		for(int i = 0; i <= 20; i++)
+		{
+			m.goto_position(1, i);
+			m.goto_position(2, i);
+			if(i == 0) sleep(5);
+			else sleep(2);
+		}
+	}
 
 	m.close();
 
